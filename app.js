cbfe3484046845d5c0023c2266fd6f52bb401c86
callmesidhu <sidharthpunalur@gmail.com>
2024-09-29 12:05:53 +0530

    index added

--- app.js
+++ app.js
@@ -3,24 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var connectDB = require('./config/connection'); 
-var userRouter = require('./routes/user');
-var accountRouter = require('./routes/account')
-var productRouter = require('./routes/products')
-var cartRouter = require('./routes/cart')
-var productDetailsRouter = require('./routes/productDetails')
-var adminRouter = require('./routes/admin')
 
+var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+var hbs = require('hbs'); // Add this line
 
 var app = express();
-var fileUpload=require('express-fileupload')
-
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs'); // Set hbs as the view engine
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,17 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.use(fileUpload())
-
-
-app.use('/', userRouter);
-app.use('/account', accountRouter)
-app.use('/products', productRouter)
-app.use('/cart', cartRouter)
-app.use('/productDetails', productDetailsRouter)
-app.use('/admin', adminRouter)
+app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
