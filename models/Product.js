0d6e038d86bf7fdee63d19764b96c1bc39ddcc76
callmesidhu <sidharthpunalur@gmail.com>
2024-09-29 22:23:35 +0530

    db time added in models

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String, required: true, trim: true },
     image: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: Number, required: true }
-});
+    price: { type: Number, required: true, min: 0 }
+}, { timestamps: true });
 
 const Product = mongoose.model('Product', productSchema);
 module.exports = Product;
