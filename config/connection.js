796926909c2ab2b2127e7b709976081cefd3f6d2
callmesidhu <sidharthpunalur@gmail.com>
2024-09-29 11:45:57 +0530

    cleaning

--- config/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/shoppingdb', {
-    });
-
-    console.log(`MongoDB Connected=>( ${conn.connection.host}:${conn.connection.port} )`);
-  }
